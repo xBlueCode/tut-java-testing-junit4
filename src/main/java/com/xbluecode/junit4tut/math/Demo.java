1809348a1875f5f8a283afb86731d910dd919f4f
xBlueCode <xbluecode@localhost.localdomain>
2018-05-06 02:33:32 +0300

    add math package

---
+++ src/main/java/com/xbluecode/junit4tut/math/Demo.java
@@ -0,0 +1,16 @@
+package com.xbluecode.junit4tut.math;
+
+public class Demo {
+    public static void main(String[] args) {
+
+        int a = 10 ;
+        int b = 0 ;
+
+        System.out.println(a +"+"+ b +" = "+ EasyMath.add(a,b));
+        System.out.println(a +"-"+ b +" = "+ EasyMath.substract(a,b));
+        System.out.println(a +"/"+ b +" = "+ EasyMath.divide(a,b));
+        System.out.println(a +" is "+ EasyMath.getSign(a));
+        System.out.println(b +" is "+ EasyMath.getSign(b));
+        System.out.println("Max Number is "+ EasyMath.max(a,b));
+    }
+}
